cd27d75652d60e306ac110dc3361dd6c18825c4d
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-01-10 19:36:01 +0530

    Update numdigits.py

--- numdigits.py
+++ numdigits.py
@@ -1,6 +1,6 @@
 n=int(input("Enter number:"))
-count=0
+di=0
 while(n>0):
-    count=count+1
+    di=di+1
     n=n//10
-print("The number of digits in the number are:",count)
+print("The number of digits in the number are:",di)
