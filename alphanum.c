f483466c368fb1bb38b2540a70dd62ec0a2b80d8
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-01-18 16:10:19 +0530

    Create alphanum.c

---
+++ alphanum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char a[200],count1=0,count2=0;
+gets(a);
+for(i=0;i!='\0';i++)
+{
+if((a=='a'&&a=='z')||(a=='A'&&a='Z')
+{
+count1++;
+}
+if(a=='0'&&a=='9')
+{
+count2++;
+}
+if(count1&&count>0)
+printf("yes");
+else
+printf("no");
+}
+}
