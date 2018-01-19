fd89f6de9f97cd5337ec739f91bcba65b0a313c9
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-01-19 08:50:46 +0000

    Create binaryreprs.c

---
+++ binaryreprs.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char a[100],i;
+printf("enter the string\n");
+scanf("%s",&a[i]);
+for(i=0;i<a[i];i++)
+{
+if(a[i]=='0'||a[i]=='1')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+}
