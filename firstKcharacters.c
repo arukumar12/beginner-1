212b532f561b8595a5f04b9fd1f378d703388ae2
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-01-20 13:27:32 +0530

    Update firstKcharacters.c

--- firstKcharacters.c
+++ firstKcharacters.c
@@ -5,4 +5,10 @@ char s[50];
 int n,i;
 printf("enter the string\n");
 scanf("%s",&s[i]);
-printf("
+printf("enter the limit\n");
+  sacnf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    printf("%c",s[i]);
+  }
+}
