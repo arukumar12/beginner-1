58388a873cdb4063d7c62c29d2020bd746233e5c
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-01-21 10:08:32 +0530

    Update composite.c

--- composite.c
+++ composite.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 void main()
 {
-int n,i,count=0;
+int m,i,count1=0;
 printf("enter the value\n");
-scanf("%d",&n);
-for(i=2;i<=(n/2);i++)
+scanf("%d",&m);
+for(i=2;i<=(m/2);i++)
 {
-if(n%i==0)
+if(m%i==0)
 {
-count++;
+count1++;
 }
-if(count==0)
+if(count1==0)
 {
 printf("prime");
 }
