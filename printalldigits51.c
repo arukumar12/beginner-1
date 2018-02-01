0780806de316dde7d7b98ce86e15971264ca1d71
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2018-02-01 14:03:44 +0530

    Create printalldigits51.c

---
+++ printalldigits51.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,s=0,r,k;
+printf("enter the digits");
+scanf("%d",&n);
+while(n!=0)
+{
+r=n%10;
+s=s*10+r;
+n=n/10;
+}
+while(s!=0)
+{
+k=s%10;
+printf("%d",k);
+s=s/10;
+}
+]
